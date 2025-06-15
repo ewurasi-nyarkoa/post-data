078f13da2a479cc2a9f4de381c57c7ab726697ef
ewurasi nyarkoa <anewurasi@gmail.com>
2025-06-15 17:41:51 +0000

    set up my post app

---
+++ src/app/component/detail-post/detail-post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-post',
+  imports: [],
+  templateUrl: './detail-post.component.html',
+  styleUrl: './detail-post.component.scss'
+})
+export class DetailPostComponent {
+
+}
---
+++ src/app/component/list-post/list-post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-post',
+  imports: [],
+  templateUrl: './list-post.component.html',
+  styleUrl: './list-post.component.scss'
+})
+export class ListPostComponent {
+
+}
