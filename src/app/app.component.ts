3f0aed940cd613a6c719171fd2dd6eecf3723151
ewurasi nyarkoa <anewurasi@gmail.com>
2025-06-16 21:52:48 +0000

    worked on my post list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,32 +2,24 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { PostService } from './service/post.service';
+import { ListPostComponent } from './component/list-post/list-post.component';
+import { Post } from './models/post.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ListPostComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
 
   title = 'post-data';
+  posts:Post[]=[];
   constructor(private myHttp:PostService) {}
 
-  // handle error gracefully in the subscription
-  // and log the data to the console
-  // ngOnInit() {
-  //   this.myHttp.getPosts().subscribe({
-  //     next: (data) => {
-  //       console.log(data);
-  //     },
-  //     error: (error) => {
-  //       console.error('Error fetching posts:', error);
-  //     }
-  //   });
-  // }  
     ngOnInit() {
       this.myHttp.getPosts().subscribe({next:(data)=>{
+        this.posts = data;
         console.log(data);
         
       },
